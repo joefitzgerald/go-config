8e17c66a20618e339f487a01096dc415e9f758ed
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2015-10-03 21:43:56 -0700

    Replace Both % In Token, Use First GOPATH Segment, Fix Tests On Windows

--- spec/pathhelper-spec.js
+++ spec/pathhelper-spec.js
@@ -6,44 +6,57 @@ import path from 'path'
 
 describe('pathhelper', () => {
   let pathhelper = null
+  let gopathToken = ''
 
   beforeEach(() => {
     runs(() => {
       pathhelper = new PathHelper()
+      gopathToken = '$GOPATH'
+      if (os.platform() === 'win32') {
+        gopathToken = '%GOPATH%'
+      }
     })
   })
 
   describe('when working with a single-item path', () => {
     it('expands the path', () => {
-      let result = pathhelper.expand(process.env, path.join('~', 'go', 'go', '..', 'bin', 'goimports'), '~/go')
+      let env = process.env
+      env.GOPATH = '~' + path.sep + 'go'
+
+      let result = pathhelper.expand(env, path.join('~', 'go', 'go', '..', 'bin', 'goimports'))
       expect(result).toBeDefined()
       expect(result).toBeTruthy()
       expect(result).toBe(path.join(pathhelper.home(), 'go', 'bin', 'goimports'))
 
-      result = pathhelper.expand(process.env, path.join('$GOPATH', 'go', '..', 'bin', 'goimports'), '~/go')
+      result = pathhelper.expand(env, path.join(gopathToken, 'go', '..', 'bin', 'goimports'))
       expect(result).toBeDefined()
       expect(result).toBeTruthy()
       expect(result).toBe(path.join(pathhelper.home(), 'go', 'bin', 'goimports'))
 
       let root = path.sep
+      let nonexistentKey = '$NONEXISTENT'
       if (os.platform() === 'win32') {
         root = 'c:' + path.sep
+        nonexistentKey = '%NONEXISTENT%'
       }
-      result = pathhelper.expand(process.env, path.join(root, '$NONEXISTENT', 'go', '..', 'bin', 'goimports'), '~/go')
+      result = pathhelper.expand(env, path.join(root, nonexistentKey, 'go', '..', 'bin', 'goimports'))
       expect(result).toBeDefined()
       expect(result).toBeTruthy()
-      expect(result).toBe(path.join(root, '$NONEXISTENT', 'bin', 'goimports'))
+      expect(result).toBe(path.join(root, nonexistentKey, 'bin', 'goimports'))
     })
   })
 
   describe('when working with a multi-item path', () => {
     it('expands the path', () => {
-      let result = pathhelper.expand(process.env, path.join('~', 'go', 'go', '..', 'bin', 'goimports'), '~/go' + path.delimiter + '~/othergo')
+      let env = process.env
+      env.GOPATH = '~' + path.sep + 'go' + path.delimiter + '~' + path.sep + 'othergo'
+
+      let result = pathhelper.expand(env, path.join('~', 'go', 'go', '..', 'bin', 'goimports'))
       expect(result).toBeDefined()
       expect(result).toBeTruthy()
       expect(result).toBe(path.join(pathhelper.home(), 'go', 'bin', 'goimports'))
 
-      result = pathhelper.expand(process.env, path.join('$GOPATH', 'go', '..', 'bin', 'goimports'), '~/go' + path.delimiter + '~/othergo')
+      result = pathhelper.expand(env, path.join(gopathToken, 'go', '..', 'bin', 'goimports'))
       expect(result).toBeDefined()
       expect(result).toBeTruthy()
       expect(result).toBe(path.join(pathhelper.home(), 'go', 'bin', 'goimports'))
