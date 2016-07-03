64a851dc7ce9580396dd192123efc5a2b2be4b6b
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-07-03 14:23:52 -0600

    Remove Dependency On The environment Package
    - Update Dependencies

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,8 +1,6 @@
 'use babel'
 /* eslint-env jasmine */
 
-import semver from 'semver'
-
 describe('go-config', () => {
   let goconfigMain = null
 
@@ -29,15 +27,12 @@ describe('go-config', () => {
       expect(goconfigMain).toBeDefined()
       expect(goconfigMain.subscriptions).toBeDefined()
       expect(goconfigMain.subscriptions).toBeTruthy()
-      expect(goconfigMain.environment).toBeDefined()
-      expect(goconfigMain.environment).toBeTruthy()
       goconfigMain.getLocator()
       expect(goconfigMain.locator).toBeDefined()
       expect(goconfigMain.locator).toBeTruthy()
 
       goconfigMain.dispose()
       expect(goconfigMain.subscriptions).toBeFalsy()
-      expect(goconfigMain.environment).toBeFalsy()
       expect(goconfigMain.locator).toBeFalsy()
 
       goconfigMain.activate()
@@ -73,41 +68,7 @@ describe('go-config', () => {
       expect(provider.locator.gopath).toBeDefined()
       expect(provider.locator.findTool).toBeDefined()
       expect(provider.locator.runtimeCandidates).not.toBeDefined()
-      expect(provider.environment).toBeDefined()
       expect(provider.environment()).toBeTruthy()
-    })
-  })
-
-  describe('when the environment is not available', () => {
-    let e = null
-    beforeEach(() => {
-      e = goconfigMain.environment
-      goconfigMain.environment = null
-    })
-    afterEach(() => {
-      goconfigMain.environment = e
-    })
-
-    it('is not ready for Atom < 1.7.0', () => {
-      expect(goconfigMain.ready).toBeDefined()
-      if (semver.satisfies(semver.major(atom.appVersion) + '.' + semver.minor(atom.appVersion) + '.' + semver.patch(atom.appVersion), '<1.7.0')) {
-        expect(goconfigMain.ready()).toBe(false)
-      }
-    })
-
-    it('returns the process environment', () => {
-      expect(goconfigMain.environment).toBeFalsy()
-      expect(goconfigMain.getEnvironment).toBeDefined()
-      expect(goconfigMain.getEnvironment()).toBeTruthy()
-      expect(goconfigMain.getEnvironment()).toBe(process.env)
-    })
-
-    it('consumes an environment', () => {
-      expect(goconfigMain.environment).toBeFalsy()
-      goconfigMain.consumeEnvironment({PING: 'PONG'})
-      expect(goconfigMain.environment).toBeTruthy()
-      expect(goconfigMain.environment.PING).toBe('PONG')
-      expect(goconfigMain.environment.PONG).not.toBeDefined()
     })
   })
 })
